1871f9c572d2268d84e0cddbcdeab3134be273b0
Luis Miguel Echevarría <144379353+CONTROLLIUM@users.noreply.github.com>
2024-02-21 23:47:56 -0500

    Add files via upload
    Primeros pasos con la FPGA Spartan 3E diseñando el hardware de la compuerta ABD

---
+++ CompuertaAnd.vhd
@@ -0,0 +1,17 @@
+library ieee ;
+    use ieee.std_logic_1164.all ;
+    use ieee.numeric_std.all ;
+
+entity ent is
+  port (
+    a : in std_logic;
+    b : in std_logic;
+    c : out std_logic
+  ) ;
+end ent ; 
+
+architecture arch of ent is
+
+begin
+c <= a and b;
+end architecture ;
